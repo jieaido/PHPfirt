673f9f1801523a39aa6f83ec36ed74a1543b9fd5
Cui-NotePC <jieaido@163.com>
2016-12-26 16:09:19 +0800

    文件类和时间类

---
+++ mysqltest.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jieaido
+ * Date: 2016/12/26
+ * Time: 14:28
+ */
+date_default_timezone_set("PRC");
+print_r(getdate()['year']);
+echo date("Y年m月d日H点i分s秒");
