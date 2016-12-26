e94039842a5f13a0f9be02dc6e8529d4f70c27da
jieai <jieiado@163.com>
2016-12-26 23:20:34 +0800

    先提交一下

--- filetest.php
+++ filetest.php
@@ -5,4 +5,16 @@
  * Date: 2016/12/26
  * Time: 16:08
  */
-haha
+
+date_default_timezone_set("asia/shanghai");
+$num=0;
+$dirname=".idea";
+$dir_handle=opendir($dirname);
+while($file=readdir($dir_handle)){
+	$dirfile=$dirname."/".$file;
+	echo $dirfile."<br>";
+	echo filesize($dirfile)."<br>";
+	echo filetype($dirfile)."<br>";
+	echo date("Y M D h:M",filemtime($dirfile)).'<br>';
+	echo "--------------------------------------".'<br>';
+}
--- mysqltest.php
+++ mysqltest.php
@@ -5,6 +5,18 @@
  * Date: 2016/12/26
  * Time: 14:28
  */
-date_default_timezone_set("PRC");
-print_r(getdate()['year']);
-echo date("Y年m月d日H点i分s秒");
+$hostname="localhost";
+$user='root';
+$pwd="root";
+$link=mysqli_connect($hostname,$user,$pwd,'testdb') or die("数据库链接失败");
+//mysqli_query($link,"SET NAMES UTF8");
+$sqlstr="select * from user";
+$result=mysqli_query($link,$sqlstr) or die("语句执行失败");
+while($row=mysqli_fetch_row($result)){
+	echo $row[0].'>';
+	echo $row[1].'>';
+	echo $row[2].'>';
+	echo $row[3].'>'."<br>";
+}
+mysqli_free_result($result);
+mysqli_close($link);
